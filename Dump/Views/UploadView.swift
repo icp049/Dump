8e069d4d75bb95e907c1ff285558ea6710641c1c
icp049 <ianpepe2000@gmail.com>
2023-06-11 18:31:40 -0600

    fixed upload view and added image picker

--- Dump/Views/UploadView.swift
+++ Dump/Views/UploadView.swift
@@ -1,25 +1,104 @@
-//
-//  UploadView.swift
-//  Dump
-//
-//  Created by Ian Pedeglorio on 2023-06-11.
-//
-
 import SwiftUI
 
 struct UploadView: View {
+    @State private var selectedImage: Image?
+    @State private var showImagePicker = false
+    @State private var caption = ""
     
     @Binding var newItemPresented: Bool
     
+    
+    var isImageSelected: Bool {
+        selectedImage != nil
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            if isImageSelected {
+                selectedImage!
+                    .resizable()
+                    .scaledToFit()
+            } else {
+                Text("Select an image")
+            }
+            
+            Button(action: {
+                showImagePicker = true
+            }) {
+                Text("Choose Image")
+            }
+            .sheet(isPresented: $showImagePicker) {
+                ImagePicker(selectedImage: $selectedImage)
+            }
+            
+            TextField("Enter caption", text: $caption)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Button(action: {
+                // Perform the post action
+                if isImageSelected {
+                    // Upload the image and caption
+                    // ...
+                }
+            }) {
+                Text("Post")
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+        }
+        .padding()
+    }
+}
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var selectedImage: Image?
+    
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+            if let uiImage = info[.originalImage] as? UIImage {
+                parent.selectedImage = Image(uiImage: uiImage)
+            }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
     }
 }
 
 struct UploadView_Previews: PreviewProvider {
     static var previews: some View {
+        
         UploadView(newItemPresented: Binding(get: {
             return true
         }, set: { _ in
-        }))    }
+        }))
+        
+    }
+    
+    
 }
+
+
+
+
+
